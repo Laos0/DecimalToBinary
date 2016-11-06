f1f5affd24fbbf58b11163da984e4f9f96fd3140
Student <Student>
2016-11-05 21:14:52 -0500

    Logic done with: if num < 0, and baseNum <2 or baseNum> 36

--- ProgrammingAssignmentTwo/Main.cpp
+++ ProgrammingAssignmentTwo/Main.cpp
@@ -2,10 +2,19 @@
 
 using namespace std;
 
+void intro();
 void decToBinary(int n);
-
+void baseFinder(int amt, int baseNumber);
 int main() {
 
+	int num = 0;
+
+	intro();
+	//baseFinder(10, 10);
+
+
+
+	/* ALLLL BELOW: CONVERTING DECIMALS TO BINARY 
 	int num = 0;
 	decToBinary(num);
 
@@ -18,13 +27,37 @@ int main() {
 	}
 	else
 	{
-		cout << endl << "Decimal " << num << endl;
+		cout << endl << "The binary for the decimal: " << num << endl;
 		decToBinary(num);
 		cout << endl << endl;
 	}
-
+	*/
 	system("pause");
 	return 0;
+
+}
+void intro() {
+	int num = 0;
+	int baseNum;
+	cout << "Enter a nonnegative integer: " << endl;
+	cin >> num;
+
+	if (num < 0) {
+		intro();
+	}
+	
+	cout << "Enter the desired base between 2 and 36." << endl;
+	cin >> baseNum;
+
+	if(baseNum < 2 || baseNum > 36){
+		cout << "Stop!" << endl;
+	}
+	
+	else {
+		decToBinary(num);
+		//cout << "Good Job!" << endl;
+	}
+	
 
 }
 
@@ -35,5 +68,13 @@ void decToBinary(int n) {
 	else {
 		decToBinary(n / 2);
 		cout << n % 2 << endl;
+	}
+}
+
+void baseFinder(int amt, int baseNumber) {
+	int result = amt / baseNumber;
+	cout << amt << endl;
+	if (result > 0) {
+		baseFinder(amt, baseNumber);
 	}
 }
